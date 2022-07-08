c8543a9aae847989ca5aecab34c5e8b6641408d3
ThyagoLucas <salc@icomp.ufam.edu.br>
2022-07-07 23:28:42 -0400

    Fix: commit with home loadin to start developement to homescreen

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 //import { useEffect, useState } from "react";
 import styled from "styled-components";
 //import api from "./api/api";
-
+import { Circles } from "react-loader-spinner";
 import Header from "./utils/Header";
 
 
@@ -13,8 +13,13 @@ function Home (){
         <Header/>
         
         <Main>
-            <button>Sou Cliente</button>
-            <button>Quero ser Cliente</button>
+
+            <div>
+                <h1>Em breve você vai poder gerenciar sua assinatura!</h1>
+                <Circles/>
+            
+            </div>
+           
         </Main>
         
        
@@ -25,11 +30,26 @@ function Home (){
 const Main = styled.main`
 
     display:flex;
-    align-items:center;
+    
     justify-content:center;
     width: 100%;
     height: 100vh;
     background-color: pink;
+
+    h1{
+        font-size: 30px;
+        color: #1A404C;
+        padding:10px;
+    }                                                   
+
+
+    div{
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        margin-top: 30px;
+    }
+   
 
 
 
